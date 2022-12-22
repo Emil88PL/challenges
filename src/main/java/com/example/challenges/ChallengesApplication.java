275ecb7db48ca2f36175effa74a56bdd6674fd5d
Emil <emil88pl@gmail.com>
2022-12-22 19:07:41 +0000

    Person

--- src/main/java/com/example/challenges/ChallengesApplication.java
+++ src/main/java/com/example/challenges/ChallengesApplication.java
@@ -1,5 +1,7 @@
 package com.example.challenges;
 
+import java.util.Scanner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,20 @@ public class ChallengesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChallengesApplication.class, args);
-	}
 
+		PersonList list = new PersonList();
+		list.addPerson(new Person("Alice", 30, "Software Engineer"));
+		list.addPerson(new Person("Bob", 35, "Manager"));
+		list.addPerson(new Person("Charlie", 40, "CEO"));
+
+		Scanner scan = new Scanner(System.in);
+		String search = scan.next();
+
+	Person person = list.findPersonByName(search);
+		if (person != null) {
+			System.out.println(person);
+		} else {
+			System.out.println("Person not found");
+		}
+	}
 }
